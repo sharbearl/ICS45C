f6c6ed56130394ad02f11670867c78ae5ac3201a
Sharon Leo <ssleo1@uci.edu>
2023-04-10 10:55:20 -0700

    Added function definitions.

--- src/letter_count.h
+++ src/letter_count.h
@@ -1,4 +1,26 @@
-int char_to_index(char ch);
-char index_to_char(int i);
-void count(string s, int counts[]);
-void print_count(int counts[], int len);
+int char_to_index(char ch)
+{
+    return int(toupper(ch)) - 65;
+}
+
+char index_to_char(int i)
+{
+    return char(i);
+}
+
+void count(string s, int counts[])
+{
+    for (int i - 0; i < length(s); i++)
+    {
+        char cur = s[i];
+        counts[char_to_index(cur)]++;
+    }
+}
+
+void print_count(int counts[], int len)
+{
+    for (int i = 0; length(counts); i++)
+    {
+        cout << index_to_char(i) << " " << counts[i] << endl;
+    }
+}
