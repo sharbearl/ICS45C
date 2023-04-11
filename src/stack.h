ff9615f7590ce98325284d502bf8b04881987163
Sharon Leo <ssleo1@uci.edu>
2023-04-11 08:48:33 -0700

    Added definitions of functions.

--- src/stack.h
+++ src/stack.h
@@ -4,15 +4,97 @@ constexpr int STK_MAX = 1000;
 class Stack
 {
 	int _top;
-    char buf[STK_MAX];
+    char buf[STK_MAX] = {};
 public:
-	Stack();
-    void push(char c);
-    char pop();
-    char top();
-    bool isEmpty();
-    bool isFull();
+	Stack()
+    {
+    	_top = 0;
+    }
+
+    void push(char c)
+    {
+    	if (!isFull())
+        {
+            buf[_top] = c;
+            _top++;
+        }
+
+    char pop()
+    {
+        char last = top();
+        if (_top == 0 && isEmpty())
+        {
+            return '@';
+        }
+        if (_top == STK_MAX)
+        {
+            buf[_top] = {};
+            _top--;
+        }
+        buf[_top] = {};
+        if (_top != 0)
+        {
+            _top--;
+        }
+        return last;
+    }
+
+    char top()
+    {
+        if (_top == STK_MAX)
+        {
+            return buf[_top - 1];
+        }
+        else
+        {
+        return buf[_top];
+        }
+    }
+
+    bool isEmpty()
+    {
+        for (int i = 0; i < STK_MAX; i++)
+        {
+            if buf[i] != '\0'
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool isFull()
+    {
+        return (_top == STK_MAX);
+    }
+
 };
 
-void push_all(Stack & stk, string line);
-void pop_all(Stack & stk);
+void push_all(Stack & stk, string line)
+{
+    for (int i = 0; i < line.length(); i++)
+    {
+        stk.push(line[i]);
+    }
+}
+void pop_all(Stack & stk)
+{
+    for (int i = 0; i < STK_MAX; i++)
+    {
+        char cur = stk.pop();
+        if (cur == '@')
+        {
+            cout << cur;
+            break;
+        }
+        if (cur != '\0')
+        {
+            cout << cur;
+        }
+        if (stk.isEmpty())
+        {
+            break;
+        }
+    }
+    cout << endl;
+}
