1d49bbd8e561901ef3b14e726ddb3eb82a2c663e
Sharon Leo <ssleo1@uci.edu>
2023-04-11 08:35:40 -0700

    Adjusted conditions of for loops.

--- src/letter_count.h
+++ src/letter_count.h
@@ -10,7 +10,7 @@ char index_to_char(int i)
 
 void count(string s, int counts[])
 {
-    for (int i - 0; i < length(s); i++)
+    for (int i = 0; i < s.length(); i++)
     {
         char cur = s[i];
         counts[char_to_index(cur)]++;
@@ -19,7 +19,7 @@ void count(string s, int counts[])
 
 void print_count(int counts[], int len)
 {
-    for (int i = 0; length(counts); i++)
+    for (int i = 0; i < 65; i++)
     {
         cout << index_to_char(i) << " " << counts[i] << endl;
     }
