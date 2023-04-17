b1d395eeba0a391aa8790d03e57c6c6d004e194b
Sharon Leo <ssleo1@uci.edu>
2023-04-16 22:50:07 -0700

    Added code for coin_simple output.

--- src/coins.cpp
+++ src/coins.cpp
@@ -6,4 +6,77 @@ Coins::Coins(int q, int d, int n, int p)
     :quarters(q), dimes(d), nickels(n), pennies(p)
 {}
 
+bool Coins::has_exact_change_for_coins(Coins const& coins) const
+{
+    return coins.quarters <= quarters && coins.dimes <= dimes && coins.nickels <= nickels && coins.pennies <= pennies;
+}
 
+Coins Coins::extract_exact_change(Coins const& coins)
+{
+    if (has_exact_change_for_coins(coins))
+    {
+        quarters -= coins.quarters;
+        dimes -= coins.dimes;
+        nickels -= coins.nickels;
+        pennies -= coins.pennies;
+
+        return coins;
+    }
+    else
+    {
+        return Coins(0,0,0,0);
+    }
+}
+
+int Coins::total_value_in_cents() const
+{
+    return (quarters * CENTS_PER_QUARTER) + (dimes * CENTS_PER_DIME) + (nickels * CENTS_PER_NICKEL) + pennies;
+}
+
+void Coins::print(std::ostream& out) const
+{
+    out << quarters << " quarters, " << dimes << " dimes, " << nickels << " nickels, " << pennies << " pennies";
+}
+
+std::ostream& operator<<(std::ostream& out, Coins const& coins)
+{
+    coins.print(out);
+    return out;
+}
+
+Coins coins_required_for_cents(int amount_in_cents)
+{
+    int q = amount_in_cents / CENTS_PER_QUARTER;
+    int q_del = q * CENTS_PER_QUARTER;
+
+    int d = (amount_in_cents - q_del) / CENTS_PER_DIME;
+    int d_del = d * CENTS_PER_DIME;
+
+    int n = (amount_in_cents - q_del - d_del) / CENTS_PER_NICKEL;
+    int n_del = n * CENTS_PER_NICKEL;
+
+    int p = (amount_in_cents - q_del - d_del - n_del);
+
+    return Coins(q, d, n, p);
+}
+
+void print_cents(int cents, std::ostream& out)
+{
+    out << "$" << cents / 100.00 << endl;
+}
+
+Coins ask_for_coins(std::istream& in, std::ostream& out)
+{
+    int q = 0, d = 0, n = 0, p = 0;
+
+    out << "Quarters? ";
+    in >> q;
+    out << "Dimes? ";
+    in >> d;
+    out << "Nickels? ";
+    in >> n; 
+    out << "Pennies? ";
+    in >> p; 
+
+    return Coins(q, d, n, p);
+}
--- src/coins_simple.cpp
+++ src/coins_simple.cpp
@@ -8,10 +8,8 @@ int main()
 {
     Coins pocket(100, 10, 10, 100);
 
-    /*
     cout << "I started with " << pocket << " in my pocket." << endl;
     Coins pay_for_candy = pocket.extract_exact_change(coins_required_for_cents(CENTS_FOR_CANDYBAR));
     cout << "I bought a candy bar for " << CENTS_FOR_CANDYBAR << " cents using " << pay_for_candy << "." << endl;
     cout << "I have " << pocket << " left in my pocket." << endl;
-    */
 }
