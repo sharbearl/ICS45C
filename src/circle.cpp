7941e8d5aed750f1dec91a3963e66454cd6f3aaf
Sharon Leo <ssleo1@uci.edu>
2023-05-14 20:06:59 -0700

    Initial draft of circle methods.

--- src/circle.cpp
+++ src/circle.cpp
@@ -1,3 +1,21 @@
 #include "circle.hpp"
 #include <iostream>
 #include <numbers>
+
+Circle::Circle(Point center, std::string name, int radius) : 
+    Shape(center, name), radius(radius) {}
+
+Circle* Circle::clone()
+{
+    return Circle(this);
+}
+
+double Circle::area() const override
+{
+    return 0.0;
+}
+
+void Circle::draw(std::ostream& out) const override
+{
+
+}
