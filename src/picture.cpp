1a51f4e14bf8cd1fd2e6cf650af509fbe8eea7ea
Sharon Leo <ssleo1@uci.edu>
2023-05-14 19:54:42 -0700

    Initial draft of picture methods.

--- src/picture.cpp
+++ src/picture.cpp
@@ -1,3 +1,20 @@
 #include "picture.hpp"
 #include <iostream>
 #include <utility>
+
+Picture::Picture() : head(nullptr), tail(nullptr){}
+
+void Picture::add(const Shape& shape)
+{
+    head = new ListNode{shape, nullptr, tail};
+    if(!tail)
+        tail = head;
+    else
+        tail->prev = head;
+}
+
+void Picture::print_all(std::ostream& out) const
+{
+    for (ListNode* p = head; p != nullptr; p = p->next)
+        p->shape.print();
+}
