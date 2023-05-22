c9739675e0c47396b70a890f3ce496b79dd9aa69
Sharon Leo <ssleo1@uci.edu>
2023-05-21 22:48:56 -0700

    Transfer code from int_array to array.

--- src/array.hpp
+++ src/array.hpp
@@ -8,8 +8,92 @@
 
 template <typename T>
 class Array {
-public:
-    //to be implemented
+public:/*
+    Array() : len{0}, buf{nullptr} {}
+
+    explicit Array(int len) : len{len}, buf{new int[len]} {}
+
+    Array(const Array& other) : len{other.len}, buf{new int[len]}
+    {    
+        copy(other.buf);
+    }
+    
+    Array(Array&& other) noexcept : len{other.len}, buf{other.buf}
+    {
+        other.len = 0;
+        other.buf = nullptr;
+    }
+
+    friend void swap(Array& lhs, Array& rhs) noexcept {
+        std::swap(lhs.len, rhs.len);
+        std::swap(lhs.buf, rhs.buf);
+    }
+
+    Array& operator=(const Array& other)
+    {
+        if(&other != this)
+        {
+            delete buf;
+            len = other.len;
+            buf = new int[len];
+
+            copy(other.buf);
+        }
+
+        return *this;
+    }
+    
+    Array& operator=(Array&& other) noexcept
+    {
+        swap(*this, other);
+        return *this;
+    }
+
+    ~Array() 
+    {
+        delete[] buf;
+    }
+
+    int length() const {
+        return len;
+    }
+
+    int& operator[](int index) {
+        if(!in_bounds(index))
+        {
+            std::ostringstream s;
+            s << "Exception operator[](" << len << ") Out Of Range";
+            throw s.str();
+        }
+        return buf[index];
+    }
+
+    const int& operator[](int index) const {
+        if(!in_bounds(index))
+        {
+            std::ostringstream s;
+            s << "Exception operator[](" << len << ") Out Of Range";
+            throw s.str();
+        }
+        return buf[index];
+    }
+
+    void fill(int val)
+    {
+        for(int i = 0; i < len; i++)
+            buf[i] = val;
+    }
+
+    void copy(int* src)
+    {
+        if(len != 0)
+        {
+            int i;
+            for(i = 0; i < len; ++i)
+                buf[i] = src[i];
+        }
+    }
+    */
     template <typename Fn>
     void fill_with_fn(Fn fn);
 
