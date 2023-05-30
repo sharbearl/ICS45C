112c17cb45c4a81443980fd2e489314b11df6806
Sharon Leo <ssleo1@uci.edu>
2023-05-30 09:58:58 -0700

    Attempt.

--- src/compute_grades.cpp
+++ src/compute_grades.cpp
@@ -12,12 +12,20 @@ bool Student::operator==(const Student& other) const;
 
 friend std::istream& Student::operator>>(std::istream& in, Student& s)
 {
-    std::string line;
-    while(getline(in, line))
+    std::string word;
+    while(word << in)
     {
         if(line = '\n')
-            break
+            break;
         else if(line.starts_with("Name")
         {
+            word << in;
+            s.first_name = word;
+            word << in;
+            s.last_name = word;
+        }
+        else if(line.starts_with("Quiz")
+        {
+            
 }
 friend std::ostream& Student::operator<<(std::ostream& out, const Student& s);
