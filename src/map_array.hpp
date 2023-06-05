0dbedb57df311699bbe16d4d94d177afe0b605ec
Sharon Leo <ssleo1@uci.edu>
2023-06-05 10:51:53 -0700

    First attempt.

--- src/map_array.hpp
+++ src/map_array.hpp
@@ -10,20 +10,44 @@
 template <typename Key, typename Value>
 class MapArray {
 public:
-    using value_type = ???;
+    using value_type = std::pair<Key, Value>;
     class ArrayIterator {
     public:
-        using iterator_category = ???;
-        using value_type = ???;
-        using difference_type = ???;
-        using pointer = ???;
-        using reference = ???;
+        using iterator_category = std::bidirectional_iterator_tag;
+        using value_type = MapArray::value_type;
+        using difference_type = std::ptrdiff_t;
+        using pointer = MapArray::value_type*;
+        using reference = MapArray::value_type&;
 
-        explicit ArrayIterator(std::pair<Key, Value>* ptr = nullptr);
-        ArrayIterator& operator++();
-        ArrayIterator& operator--();
-        ArrayIterator operator++(int);
-        ArrayIterator operator--(int);
+        explicit ArrayIterator(std::pair<Key, Value>* ptr = nullptr)
+            : ptr(ptr) {}
+
+        ArrayIterator& operator++()
+        {
+           ++ptr;
+           return *this;
+        }
+
+        ArrayIterator& operator--()
+        {
+            --ptr;
+            return *this;
+        }
+
+        ArrayIterator operator++(int)
+        {
+            auto cpy = *this;
+            ptr++;
+            return cpy;
+        }
+
+        ArrayIterator operator--(int)
+        {
+            auto cpy = *this;
+            ptr++;
+            return cpy;
+        }
+
         ArrayIterator& operator+=(difference_type d);
         ArrayIterator& operator-=(difference_type d);
         friend ArrayIterator operator+(ArrayIterator it, difference_type d);
@@ -39,8 +63,16 @@ public:
         std::pair<Key, Value>* ptr;
     };
     using iterator = ArrayIterator;
-    ArrayIterator begin();
-    ArrayIterator end();
+    ArrayIterator begin()
+    {
+        return ArrayIterator(data.begin());
+    }
+
+    ArrayIterator end()
+    {
+        return ArrayIterator(nullptr);
+    }
+
     Value& operator[](const Key& key);
 private:
     std::vector<std::pair<Key, Value>> data;
