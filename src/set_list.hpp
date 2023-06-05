b3443b36fd23efa10d8690ecd06887b6c8548fe2
Sharon Leo <ssleo1@uci.edu>
2023-06-05 10:54:29 -0700

    Made changes to iterator traits.

--- src/set_list.hpp
+++ src/set_list.hpp
@@ -21,8 +21,8 @@ public:
         using iterator_category = std::forward_iterator_tag;
         using value_type = SetList::value_type;
         using difference_type = std::ptrdiff_t;
-        using pointer = T*;
-        using reference = T&;
+        using pointer = SetList::value_type*;
+        using reference = SetList::value_type&;
 
         explicit ListIterator(std::shared_ptr<ListNode> ptr = nullptr)
             : ptr(ptr) {}
